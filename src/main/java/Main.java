97f3c00b896aa614686186beb1ec9f09a7b08c09
Szymon <szymon.nedzi@gmail.com>
2020-03-28 17:48:58 +0100

    Remove absolute path usage in favor of relative path

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class Main {
@@ -12,12 +14,15 @@ public class Main {
         System.out.println("Hello World!");
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
-        TypeReference<List<Appointment>> typeReference = new TypeReference<List<Appointment>>() {
-        };
-        File file = new File("C:\\Users\\Darek\\Desktop\\PatientSkyTest\\src\\main\\resources\\CalendarJsons\\Danny boy appointments.json");
+        TypeReference<List<Appointment>> typeReference = new TypeReference<List<Appointment>>(){};
+        Path currentDir = Paths.get("");
+        Path targetDir = Paths.get(currentDir + "src/main/resources/CalendarJsons/Danny boy appointments.json");
+        File file = new File(String.valueOf(targetDir));
         List<Appointment> appointments = objectMapper.readValue(file, typeReference);
         for (Appointment appointment : appointments) {
             System.out.println(appointment);
         }
+
+
     }
 }
