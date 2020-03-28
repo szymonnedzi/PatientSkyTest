63af0d8672fc301f83d0cecc4afadf3efbbcd983
Szymon <szymon.nedzi@gmail.com>
2020-03-28 16:19:57 +0100

    Empty commit - pause.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,9 +8,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         System.out.println("Hello World!");
         ObjectMapper objectMapper = new ObjectMapper();
-
         File file = new File("C:\\Users\\Darek\\Desktop\\PatientSkyTest\\src\\main\\resources\\CalendarJsons\\Danny boy.json");
-
         Calendar calendar = objectMapper.readValue(file, Calendar.class);
 
     }
