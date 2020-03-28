5b2774095d181e9b2c60373fda414ae1ce07d9ce
Szymon <szymon.nedzi@gmail.com>
2020-03-28 23:15:40 +0100

    Fresh take on UUID creating

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -28,20 +28,24 @@ public class Main {
         TypeReference<Calendar> calendarTypeReference = new TypeReference<Calendar>() {
         };
 
-        //Prepare the data
+        //Prepare the data - json locations
         Path dannyPath = Paths.get(projectDir + "src/main/resources/CalendarJsons/Danny boy.json");
         Path emmaPath = Paths.get(projectDir + "src/main/resources/CalendarJsons/Emma Win.json");
         Path joannaPath = Paths.get(projectDir + "src/main/resources/CalendarJsons/Joanna Hef.json");
+
+        //File fetching
         File file2 = new File(String.valueOf(dannyPath));
         File file3 = new File(String.valueOf(emmaPath));
         File file4 = new File(String.valueOf(joannaPath));
+
+
+        //Calendar mapping
         Calendar calendar2 = objectMapper.readValue(file2, calendarTypeReference);
         calendar2.setCalendarID(createCalendarUUID("Danny boy"));
         Calendar calendar3 = objectMapper.readValue(file3, calendarTypeReference);
         calendar3.setCalendarID(createCalendarUUID("Emma Win"));
         Calendar calendar4 = objectMapper.readValue(file4, calendarTypeReference);
         calendar4.setCalendarID(createCalendarUUID("Joanna Hef"));
-
 
         //Generate list of calendars.
         List<Calendar> calendars = new ArrayList<>();
@@ -55,7 +59,16 @@ public class Main {
     }
 
     static UUID createCalendarUUID(String calendarOwner) {
-        return UUID.fromString(UUID.nameUUIDFromBytes(calendarOwner.getBytes()).toString());
+        if (calendarOwner.equals("Danny Boy")) {
+            return UUID.fromString("48cadf26-975e-11e5-b9c2-c8e0eb18c1e9");
+        }
+        if (calendarOwner.equals("Emma Win")) {
+            return UUID.fromString("452dccfc-975e-11e5-bfa5-c8e0eb18c1e9");
+        }
+        if (calendarOwner.equals("Joanna Hef")) {
+            return UUID.fromString("48644c7a-975e-11e5-a090-c8e0eb18c1e9");
+        } else return UUID.fromString(UUID.nameUUIDFromBytes(calendarOwner.getBytes()).toString());
     }
+
     //findAvailableTime(array<Uuid> calendarIds, Integer duration, Iso8601TimeInterval periodToSearch [,Uuid timeSlotType]);
 }
