a4453ff594901a02e87bc5e218d8d9130f6c4a83
Szymon <szymon.nedzi@gmail.com>
2020-03-28 17:41:53 +0100

    Properly read in Appointments using Jackson

--- src/main/java/Appointment.java
+++ src/main/java/Appointment.java
@@ -1,3 +1,4 @@
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.UUID;
 
@@ -6,14 +7,127 @@ public class Appointment {
     UUID id;
     UUID patient_id;
     UUID calendar_id;
-    Date startTime;
-    Date endTime;
+    Date start;
+    Date end;
     String patient_comment;
     String note;
     UUID time_slot_type_id;
+    UUID type_id;
     Integer state;
     String out_of_office_location;
     Boolean out_of_office;
-    Boolean complete;
+    Boolean completed;
     Boolean is_scheduled;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public UUID getPatient_id() {
+        return patient_id;
+    }
+
+    public void setPatient_id(UUID patient_id) {
+        this.patient_id = patient_id;
+    }
+
+    public UUID getCalendar_id() {
+        return calendar_id;
+    }
+
+    public void setCalendar_id(UUID calendar_id) {
+        this.calendar_id = calendar_id;
+    }
+
+    public Date getStart() {
+        return start;
+    }
+
+    public void setStart(Date start) {
+        this.start = start;
+    }
+
+    public Date getEnd() {
+        return end;
+    }
+
+    public void setEnd(Date end) {
+        this.end = end;
+    }
+
+    public String getPatient_comment() {
+        return patient_comment;
+    }
+
+    public void setPatient_comment(String patient_comment) {
+        this.patient_comment = patient_comment;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public UUID getTime_slot_type_id() {
+        return time_slot_type_id;
+    }
+
+    public void setTime_slot_type_id(UUID time_slot_type_id) {
+        this.time_slot_type_id = time_slot_type_id;
+    }
+
+    public UUID getType_id() {
+        return type_id;
+    }
+
+    public void setType_id(UUID type_id) {
+        this.type_id = type_id;
+    }
+
+    public Integer getState() {
+        return state;
+    }
+
+    public void setState(Integer state) {
+        this.state = state;
+    }
+
+    public String getOut_of_office_location() {
+        return out_of_office_location;
+    }
+
+    public void setOut_of_office_location(String out_of_office_location) {
+        this.out_of_office_location = out_of_office_location;
+    }
+
+    public Boolean getOut_of_office() {
+        return out_of_office;
+    }
+
+    public void setOut_of_office(Boolean out_of_office) {
+        this.out_of_office = out_of_office;
+    }
+
+    public Boolean getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(Boolean completed) {
+        this.completed = completed;
+    }
+
+    public Boolean getIs_scheduled() {
+        return is_scheduled;
+    }
+
+    public void setIs_scheduled(Boolean is_scheduled) {
+        this.is_scheduled = is_scheduled;
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,23 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         System.out.println("Hello World!");
         ObjectMapper objectMapper = new ObjectMapper();
-        File file = new File("C:\\Users\\Darek\\Desktop\\PatientSkyTest\\src\\main\\resources\\CalendarJsons\\Danny boy.json");
-        Calendar calendar = objectMapper.readValue(file, Calendar.class);
+        objectMapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
+        TypeReference<List<Appointment>> typeReference = new TypeReference<List<Appointment>>() {
+        };
+        File file = new File("C:\\Users\\Darek\\Desktop\\PatientSkyTest\\src\\main\\resources\\CalendarJsons\\Danny boy appointments.json");
+        List<Appointment> appointments = objectMapper.readValue(file, typeReference);
+        for (Appointment appointment : appointments) {
+            System.out.println(appointment);
+        }
     }
 }
